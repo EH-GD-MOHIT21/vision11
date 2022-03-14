74d3a83416d4c22a7f67d69b72cd410d79e29660
Mohit Satija <experimentallyf@gmail.com>
2022-03-15 00:06:18 +0530

    landing page

--- mainAPP/views.py
+++ mainAPP/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def RenderHomePage(request):
+    '''
+    This method is used for
+    rendering home page.
+    '''
+    return render(request,'home.html')
