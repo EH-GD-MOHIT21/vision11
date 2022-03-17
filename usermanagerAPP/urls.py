1e1b14859895b0b978b7d7ca1b48abd8eb69b425
Mohit Satija <experimentallyf@gmail.com>
2022-03-17 20:01:36 +0530

    nothing

--- usermanagerAPP/urls.py
+++ usermanagerAPP/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login',views.RenderLoginPage),
+    path('',views.RenderLoginPage),
 ]
--- usermanagerAPP/views.py
+++ usermanagerAPP/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def RenderLoginPage(request):
-    return render(request,'home.html')
+    return render(request,'vision_login.html')
+
