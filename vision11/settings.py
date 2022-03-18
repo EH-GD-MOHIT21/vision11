7284e087bf45b8497b8fc0bc8baf9379ad440543
Mohit Satija <experimentallyf@gmail.com>
2022-03-19 00:28:39 +0530

    config acc to heroku

--- vision11/settings.py
+++ vision11/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-_%m7ea3s-j7c!%#$1kcr5rd9l%v0+3r2)kfokng)=fl!i992(h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
