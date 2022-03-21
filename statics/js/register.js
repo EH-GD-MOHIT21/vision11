bdeefd33dc8b4c3d06c44eb3cb820ed9dbfda9e5
Mohit Satija <experimentallyf@gmail.com>
2022-03-22 04:13:24 +0530

    updated signup.html

--- statics/js/register.js
+++ statics/js/register.js
@@ -6,6 +6,8 @@ var showcontent = 2;
 var start = 0;
 var finish = showcontent;
 document.getElementById('prevbtn').style.display = 'none';
+var emailstatus = undefined;
+var usernamestatus = undefined;
 
 for(var i=showcontent;i<lengths;i++){
     inputs[i].style.display = 'none';
@@ -43,9 +45,9 @@ function next(start,finish){
     }
 }
 
-function showerror(id,message=1){
+function showerror(id,message=1,custommsg="This Field is required!!"){
     if(message==1)
-    document.getElementById("msg-error-"+(id+1)).innerText = "This Field is required!!";
+    document.getElementById("msg-error-"+(id+1)).innerText = custommsg;
     else
     document.getElementById("msg-error-"+(id+1)).innerText = "";
 }
@@ -54,10 +56,26 @@ document.getElementById('nextbtn').addEventListener('click',function(){
     for(var i=start;i<finish;i++){
         showerror(i,message=0);
     }
-    for(var i=start;i<finish;i++){
-        if(is_null(elm(i))){
-            showerror(i);
+
+    if(finish==4){
+        // particular checks
+        // showerror takes id-1 as input as it coverts id to id+1 
+        if(emailstatus===undefined){
+            showerror(2,message=1,custommsg="Invalid mail or already taken!!!")
             return;
+        }
+        if(usernamestatus === undefined){
+            showerror(3,message=1,custommsg="Invalid username or already taken!!!")
+            return;
+        }
+    }
+    else{
+        // default check for null
+        for(var i=start;i<finish;i++){
+            if(is_null(elm(i))){
+                showerror(i);
+                return;
+            }
         }
     }
     if(finish==inputelmslength){
@@ -94,3 +112,60 @@ document.getElementById('prevbtn').addEventListener('click',function(){
 })
 
 document.getElementById('signupform').style.display = 'block';
+
+// axios post -> username
+document.getElementById("id_username").addEventListener("input", function() {
+    var uname = document.getElementById("id_username").value;
+    axios.post('/check/username', {
+            'username': uname,
+        })
+        .then(res => {
+            let unamestatus = res['data']['message'];
+            if (res['data']['message'] == 'Username Available.') {
+                document.getElementById("msg-error-4").textContent = unamestatus;
+                document.getElementById("msg-error-4").style.color = "green";
+                usernamestatus = true;
+            } else {
+                document.getElementById("msg-error-4").textContent = unamestatus;
+                document.getElementById("msg-error-4").style.color = "crimson";
+                usernamestatus = undefined;
+            }
+
+        });
+});
+
+// axios post -> email
+document.getElementById("id_email").addEventListener("input", function() {
+    var email = document.getElementById("id_email").value;
+    axios.post('/check/email', {
+            'email': email,
+        })
+        .then(res => {
+            unamestatus = res['data']['message'];
+            if (res['data']['message'] == 'Email Available.') {
+                document.getElementById("msg-error-3").textContent = unamestatus;
+                document.getElementById("msg-error-3").style.color = "green";
+                emailstatus = true;
+            } else {
+                document.getElementById("msg-error-3").textContent = unamestatus;
+                document.getElementById("msg-error-3").style.color = "crimson"
+                emailstatus = undefined;
+            }
+
+        });
+});
+
+document.getElementById('boyimg').addEventListener('click',function(){
+    document.getElementById('id_gender_type').value = 'Male';
+    document.getElementById('boyimg').style.border = "1px solid blue";
+    document.getElementById('girlimg').style.border = "none";
+})
+
+
+document.getElementById('girlimg').addEventListener('click',function(){
+    document.getElementById('id_gender_type').value = 'Female';
+    document.getElementById('girlimg').style.border = "1px solid blue";
+    document.getElementById('boyimg').style.border = "none";
+})
+
+document.getElementById('boyimg').style.border = "1px solid blue";
--- usermanagerAPP/views.py
+++ usermanagerAPP/views.py
@@ -3,7 +3,7 @@ from .models import User1
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_200_OK,HTTP_406_NOT_ACCEPTABLE,HTTP_400_BAD_REQUEST
-
+import re
 
 
 class CheckForUsername(APIView):
@@ -13,9 +13,9 @@ class CheckForUsername(APIView):
             User1.objects.get(username=data["username"])
             return Response({'status':HTTP_406_NOT_ACCEPTABLE,'message':'Username not available.'})
         except User1.DoesNotExist:
-            if len(data["username"]):
+            if len(data["username"]) and data["username"].isalnum():
                 return Response({'status':HTTP_200_OK,'message':'Username Available.'})
-            return Response({'status':HTTP_200_OK,'message':'Please enter username.'})
+            return Response({'status':HTTP_200_OK,'message':'Please enter valid username of alpha num values.'})
         except:
             return Response({'status':HTTP_400_BAD_REQUEST,'message':'Invalid Request.'})
 
@@ -24,11 +24,12 @@ class CheckForEmail(APIView):
     def post(self,request,*args):
         data = request.data
         try:
+            regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+            if not (re.fullmatch(regex, data["email"])):
+                return Response({'status':HTTP_200_OK,'message':'Please enter valid Email.'})
             User1.objects.get(email=data["email"])
             return Response({'status':HTTP_406_NOT_ACCEPTABLE,'message':'Email not available.'})
         except User1.DoesNotExist:
-            if len(data["email"]):
-                return Response({'status':HTTP_200_OK,'message':'Email Available.'})
-            return Response({'status':HTTP_200_OK,'message':'Please enter Email.'})
-        except:
             return Response({'status':HTTP_200_OK,'message':'Email Available.'})
+        except:
+            return Response({'status':HTTP_400_BAD_REQUEST,'message':'Invalid Email.'})
