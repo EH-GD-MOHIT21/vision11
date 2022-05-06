5462fb1fbcade3adeca14af7a74ceea3e5af45b0
gopikrishna28 <gopikrishna1470@gmail.com>
2022-05-07 00:44:24 +0530

    db.sqlite3

--- mainAPP/admin.py
+++ mainAPP/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Match,Team,PlayersMatchData, User_Feature_Suggestion
+from .models import Match,Team,PlayersMatchData, User_Feature_Suggestion,Player
 
 
 admin.site.register(Match)
@@ -10,3 +10,4 @@ admin.site.register(Team)
 admin.site.register(PlayersMatchData)
 
 admin.site.register(User_Feature_Suggestion)
+admin.site.register(Player)
