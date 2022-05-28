b30a1005931a6947a4cc8aa4d7b0bb1550234de8
hks74123 <hemant91852@gmail.com>
2022-05-28 17:37:55 +0530

    showing of multiple winners

--- mainAPP/admin.py
+++ mainAPP/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Match,Team,PlayersMatchData, User_Feature_Suggestion,Player,UserTeam,Contest
-
+from django import forms
 
 class CustomMatch(admin.ModelAdmin):
     list_display = ["title","team1", "team2", "time"]
@@ -13,6 +13,11 @@ class CustomContest(admin.ModelAdmin):
     list_display = ["match_id","entry_fee","fee_type","reward_claimed","length","price_fee"]
     search_fields = ["match_id__title","entry_fee","fee_type","reward_claimed","length","price_fee"]
 
+    def get_form(self,request,obj=None,**kwargs):
+        kwargs['widgets']={
+            'price_distribution_array':forms.Textarea(attrs={'placeholder':'An array of like [0.3,0.2,0.2,0.2,0.1,0] for a contest of length 6 or pass strings seprated by enter in forms of 0-1:0.2   for 1st and 2nd player stack will be 0.1 each total 0.2 of total price money'})
+        }
+        return super().get_form(request,obj,**kwargs)
 
 
 class CustomPlayer(admin.ModelAdmin):
