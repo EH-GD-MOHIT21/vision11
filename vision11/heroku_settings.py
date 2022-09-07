c1e7e6e99f4e1c902e0275dca4b40a8eb232ddf9
Mohit Satija <experimentallyf@gmail.com>
2022-09-08 03:47:47 +0530

    fixed issue: mail not sent

--- vision11/heroku_settings.py
+++ vision11/heroku_settings.py
@@ -1,6 +1,8 @@
 from celery.schedules import crontab
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -167,8 +169,8 @@ SOCIALACCOUNT_PROVIDERS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "no.reply.python.py@gmail.com"
-EMAIL_HOST_PASSWORD = "qwerty@123"
+EMAIL_HOST_USER = "no.reply.mohit.apps@gmail.com"
+EMAIL_HOST_PASSWORD = os.getenv("PASS_MAIL")
 EMAIL_USE_TLS = True
 
 
